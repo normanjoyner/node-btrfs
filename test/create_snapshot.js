096eeb2116521f0e1ed7b2636e2b1fd301eb9be8
Norman Joyner <norman.joyner@gmail.com>
2015-10-24 23:20:32 -0400

    improve test

--- test/create_snapshot.js
+++ test/create_snapshot.js
@@ -5,8 +5,12 @@ module.exports = {
 
     start: function(){
         describe("create_snapshot", function(){
+            before(function(fn){
+                btrfs.create_subvolume("/mnt/btrfs/test_create_snapshot", fn);
+            });
+
             it("should create a btrfs snapshot", function(fn){
-                btrfs.create_snapshot("/mnt/btrfs/testvol", "/mnt/btrfs/testsnapshot", { readonly: true }, fn);
+                btrfs.create_snapshot("/mnt/btrfs/test_create_snapshot", "/mnt/btrfs/newly_created_snapshot", { readonly: true }, fn);
             });
         });
     }
--- test/delete_subvolume.js
+++ test/delete_subvolume.js
@@ -5,6 +5,10 @@ module.exports = {
 
     start: function(){
         describe("delete_subvolume", function(){
+            before(function(fn){
+                btrfs.create_subvolume("/mnt/btrfs/test_delete_subvolume", fn);
+            });
+
             it("should delete a btrfs subvolume", function(fn){
                 btrfs.delete_subvolume("/mnt/btrfs/test_delete_subvolume", fn);
             });
